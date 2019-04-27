ffff8ddb9e9f41c37e008274ba77510ae5dfad9e
GuillaumeSarfati <guillaume.sarfati@studet.42.fr>
2019-04-27 07:01:01 +0200

    [ FEATURE ] Eject

---
+++ ios/popeye/AppDelegate.m
@@ -0,0 +1,16 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#import "AppDelegate.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+@implementation AppDelegate
+
+// Put your app delegate methods here. Remember to also call methods from EXStandaloneAppDelegate superclass
+// in order to keep Expo working. See example below.
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
